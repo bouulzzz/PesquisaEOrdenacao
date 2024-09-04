8f164bcb5ba87557da7601f67ed28274d9babfeb
brunodifante <134736986+bouulzzz@users.noreply.github.com>
2024-09-04 08:52:23 -0300

    Update Main.py

--- pesquisaeordenacao-metodos/Main.py
+++ pesquisaeordenacao-metodos/Main.py
@@ -15,31 +15,27 @@ quantidade = 60000
 
 tamanhoNome = random.randint(3, 6)
 
+Util.geradorParaLista(lista_sort, quantidade, tamanhoNome)
 Util.geradorParaLista(lista_bolha, quantidade, tamanhoNome)
 Util.geradorParaLista(lista_selecao, quantidade, tamanhoNome)
 Util.geradorParaLista(lista_insercao, quantidade, tamanhoNome)
-Util.geradorParaLista(lista_sort, quantidade, tamanhoNome)
 
+print("\n Lista com método sort \n")
+lista_sort = sorted(lista_sort)
+for aluno in lista_sort:
+    print(aluno)
 
 print("\n Lista com método bolha \n")
 Ordenacao.Bolha(lista_bolha)
 for aluno in lista_bolha:
     print(aluno)
 
-
 print("\n Lista com método seleção \n")
 Ordenacao.Selecao(lista_selecao)
 for aluno in lista_selecao:
     print(aluno)
 
-
 print("\n Lista com método inserção \n")
 Ordenacao.Insercao(lista_insercao)
 for aluno in lista_insercao:
-    print(aluno)
-
-
-print("\n Lista com método sort \n")
-lista_sort = sorted(lista_sort)
-for aluno in lista_sort:
     print(aluno)
